743986c05ffba140f2cb115905fdd27bce273714
Lachezar Tsochev <ltsochev@live.com>
2025-01-22 18:01:20 +0200

    fixed tailwind

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello World</h2>);
+root.render(
+  <div className="flex">
+    <h1 className="text-3xl">💖 Hello World!</h1>
+    <p className="text-xl">Welcome to your Electron application.</p>
+  </div>
+);
